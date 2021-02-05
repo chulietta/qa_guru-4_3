345a1acce0a6c55c0873ac2c497e1b5adc912c13
Yuliya <9757975@gmail.com>
2021-02-05 20:26:07 +0300

    edit class name

--- src/test/java/github/FindExamplesInWikiTests.java
+++ src/test/java/github/FindExamplesInWikiTests.java
@@ -1,13 +1,12 @@
 package github;
 
-import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
-public class findExamplesInWikiTests {
+public class FindExamplesInWikiTests {
 
     @Test
     @DisplayName("Find JUnit5 example in Wiki")
